2562dba1309774b6e34c3f586293bbc0858a2ae3
Marijn17S <59451866+Marijn17S@users.noreply.github.com>
2023-06-06 13:02:23 +0200

    fixed create

--- lib/Database/creategame.dart
+++ lib/Database/creategame.dart
@@ -3,17 +3,19 @@ import 'package:eindopdracht5/token_manager.dart';
 import 'dart:convert';
 
 Future<void> createGame(Map<String, dynamic> gameData) async {
+  final body = {
+    'name': gameData['name'], // Get the name from the game data
+    'rating': gameData['rating'].toString(), // Get the rating from the game data
+    'released': gameData['released'], // Get the release date from the game data
+    'description_raw': gameData['description_raw'], // Get the description from the game data
+    'platforms': gameData['platforms'].map((platform) => platform['platform']['name']).toList().join(','), // Get the platforms as a comma-separated string
+    'genres': gameData['genres'].map((genre) => genre['name']).toList().join(','), // Get the genres as a comma-separated string
+    'developers': gameData['developers'].map((developer) => developer['name']).toList().join(','), // Get the developers as a comma-separated string
+  };
+  print(jsonEncode(body));
   final response = await http.post(
     Uri.parse('${TokenManager.baseApi}/games'),
-    body: {
-      'name': gameData['name'], // Get the name from the game data
-      'rating': gameData['rating'].toString(), // Get the rating from the game data
-      'released': gameData['released'], // Get the release date from the game data
-      'description_raw': gameData['description_raw'], // Get the description from the game data
-      'platforms': gameData['platforms'].map((platform) => platform['platform']['name']).toList().join(','), // Get the platforms as a comma-separated string
-      'genres': gameData['genres'].map((genre) => genre['name']).toList().join(','), // Get the genres as a comma-separated string
-      'developers': gameData['developers'].map((developer) => developer['name']).toList().join(','), // Get the developers as a comma-separated string
-    },
+    body: jsonEncode(body),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
       'Authorization': 'Bearer ${TokenManager.bearerToken}'
